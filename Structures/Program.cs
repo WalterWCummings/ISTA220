df491ee7279ec4307e46bc0e5b03c3cc9263d598
Walter Cummings <77279470+WalterWCummings@users.noreply.github.com>
2021-04-24 23:55:11 -0400

    Add files via upload

---
+++ Structures/Program.cs
@@ -0,0 +1,66 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace StructsAndEnums
+{
+    class Program
+    {
+        static void doWork()
+        {
+            Console.WriteLine("This is chapter 9 lab.");
+            Month first = Month.January;
+            Console.WriteLine(first);
+            Console.WriteLine((int) first);        
+            first++;
+            Console.WriteLine(first);
+            Console.WriteLine((int)first);
+            first += 10;
+            Console.WriteLine(first);
+            Console.WriteLine((int)first);
+            first++;
+            Console.WriteLine(first);
+            Console.WriteLine((int)first);
+
+            Month last = Month.Cesarary;
+            Console.WriteLine(last);
+            last += 1;
+            Console.WriteLine(last);
+
+            Console.WriteLine("----------");
+
+            Date defaultDate = new Date();
+            Console.WriteLine(defaultDate);
+
+            Date weddingAnniversary = new Date(2018, Month.December, 28);
+            Console.WriteLine(weddingAnniversary);
+
+            Date weddingAnniversaryCopy = weddingAnniversary;
+            Console.WriteLine($"Value of copy is {weddingAnniversaryCopy}");
+
+            weddingAnniversary.AdvanceMonth();
+            Console.WriteLine($"New value of weddingAnniversary is {weddingAnniversary}");
+            Console.WriteLine($"Value of copy is still {weddingAnniversary}");
+
+
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
